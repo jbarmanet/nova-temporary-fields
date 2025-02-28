f3ff76cb343f6b186d8a9288eedd5980a3e11d18
Jean-Baptiste Armanet <jb.armanet@gmail.com>
2025-02-28 12:49:31 +0100

    Fix Nova 5

--- src/HasTemporaryFields.php
+++ src/HasTemporaryFields.php
@@ -7,10 +7,10 @@ use Laravel\Nova\Http\Requests\NovaRequest;
 
 trait HasTemporaryFields
 {
-    protected static function fillFields(NovaRequest $request, $model, Collection $fields)
+    protected static function fillFields(NovaRequest $request, $model, Collection $fields): array
     {
         return parent::fillFields($request, $model, $fields->reject(function ($field) {
             return $field->meta['_temp'] ?? false;
-        }));
+        }))->all();
     }
 }

322baf138df29072fa0c487beaa50590131fc023
Jean-Baptiste Armanet <jb.armanet@gmail.com>
2025-02-28 12:45:53 +0100

    Fix for Nova5

--- src/HasTemporaryFields.php
+++ src/HasTemporaryFields.php
@@ -2,11 +2,12 @@
 
 namespace Ganyicz\NovaTemporaryFields;
 
+use Illuminate\Support\Collection;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 trait HasTemporaryFields
 {
-    protected static function fillFields(NovaRequest $request, $model, $fields)
+    protected static function fillFields(NovaRequest $request, $model, Collection $fields)
     {
         return parent::fillFields($request, $model, $fields->reject(function ($field) {
             return $field->meta['_temp'] ?? false;
